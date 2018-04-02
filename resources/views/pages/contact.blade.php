85395d89d3d801047dbf5ffd6611d5efa9204cb8
Reyn Ackon <reynackon@gmail.com>
2018-04-02 13:00:39 -0400

    Contact me page

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,43 @@
+@include('includes.head')
+
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            @endauth
+        </div>
+    @endif
+
+<div class="container">
+    <h1 class="mb-2 text-center">Contact Us</h1>
+
+
+
+    <div class="col-12 col-md-6">
+        <form class="form-horizontal">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="Name">Name: </label>
+                <input type="text" class="form-control" id="name" placeholder="Your name" name="name" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email: </label>
+                <input type="text" class="form-control" id="email" placeholder="john@example.com" name="email" required>
+            </div>
+
+            <div class="form-group">
+                <label for="message">message: </label>
+                <textarea type="text" class="form-control luna-message" id="message" placeholder="Type your messages here" name="message" required></textarea>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" value="Send">Send</button>
+            </div>
+        </form>
+    </div>
+</div> <!-- /container -->

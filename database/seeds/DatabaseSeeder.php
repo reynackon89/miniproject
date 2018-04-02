97e2f79512e0ad7efc20eaaa90805d0ab60d4154
Reyn Ackon <reynackon@gmail.com>
2018-04-02 13:43:26 -0400

    DB seed to generate 50 cars.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(CarsTableSeeder::class);
+        //Database seeds have been created
     }
 }
